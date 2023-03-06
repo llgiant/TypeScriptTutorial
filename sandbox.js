7e7266b9b5a71bb76443848f1e5d6ff4043eff75
Bogdan <Meln>
2023-03-06 12:13:35 +0600

    ts to js

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,26 @@
 var character = 'mario';
 var age = 30;
 var isBlackBelt = false;
+var inputs = document.querySelectorAll('input');
 // character = 20;
 character = 'luigi';
 // age = 'yoshi';
 age = 40;
 // isBlackBelt = 'yes';
 isBlackBelt = true;
-var area = function (diameter) {
+var circ = function (diameter) {
     return diameter * Math.PI;
 };
+var sqrt = function (sqrtNumber) {
+    return Math.sqrt(sqrtNumber);
+};
+var max = function (first, second, third) {
+    return sqrt(Math.max(first, second, third));
+};
 // console.log(area('hello'));
-console.log(area(7.5));
+console.log(circ(7.5).toFixed(2));
+console.log(sqrt(28).toFixed(2));
+console.log(max(26, 28, 28).toFixed(2));
+inputs.forEach(function (input) {
+    console.log(input);
+});
--- sandbox.ts
+++ sandbox.ts
@@ -1,6 +1,8 @@
 let character = 'mario';
 let age = 30;
 let isBlackBelt = false;
+const inputs = document.querySelectorAll('input');
+
 
 // character = 20;
 character = 'luigi';
@@ -15,5 +17,20 @@ const circ = (diameter: number) => {
   return diameter * Math.PI;
 };
 
+const sqrt = (sqrtNumber : number) => {
+  return Math.sqrt(sqrtNumber);
+}
+
+const max = (first:number,second: number,third : number) => {
+  return sqrt(Math.max(first,second,third));
+}
 // console.log(area('hello'));
-console.log(circ(7.5));
+console.log(circ(7.5).toFixed(2));
+
+console.log(sqrt(28).toFixed(2));
+
+console.log(max(26,28,28).toFixed(2));
+
+inputs.forEach(input => {
+  console.log(input);
+});
